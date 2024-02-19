1b3de0f99028ca5e913400379caa6fef8f628b30
Carlos <carloskarat@gmail.com>
2024-02-19 15:31:15 +0100

    update

--- övningsmat/test1.py
+++ övningsmat/test1.py
@@ -5,7 +5,7 @@ import gzip
 import blackboxprotobuf  # Assuming blackboxprotobuf has been successfully installed
 
 
-filePath = ""
+filePath = "C:/Users/TheKi/Downloads/Telefon2-GalaxyS21/CellebriteCTF23SharonGalaxyS21.zip"
 tempFolder = "./temp"
 
 openZIP = zipfile.ZipFile(filePath, mode="r")
@@ -14,3 +14,13 @@ for filename in openZIP.namelist():
     if "NoteStore.sqlite" in filename:
         print("NotesStore found: ", filename)
         localFileName = filename.rsplit("/", 1)[-1]
+        print("Saving file: ", localFileName)
+        with open(os.path.join(tempFolder, localFileName), 'wb') as localFile:
+            localFile.write(openZIP.read(filename))
+            
+sqliteDatabase = os.path.join(tempFolder, "NoteStore.sqlite")
+
+sqliteConnection=sqlite3.connect(sqliteDatabase)
+
+sqliteCursor = sqliteConnection.cursor()
+        
